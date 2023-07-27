53516f2c40cd5f15426486efaef63928c39f031b
Mattias Lundkvist <mindfalls@hotmail.com>
2023-07-27 16:41:53 +0000

    define no_of_players function

--- run.py
+++ run.py
@@ -6,3 +6,19 @@ def roll():
     roll = random.randint(min_value, max_value)
 
     return roll
+def no_of_players():
+    while True:
+        players = input("Enter number of players (1-4) ")
+        if players.isdigit():
+            players = int(players)
+            if 1 <= players <=4:
+                break
+            else:
+                print("Must be between 1 -4.")
+        else:
+            print("invalid, try again")
+
+    print(players)
+
+
+
