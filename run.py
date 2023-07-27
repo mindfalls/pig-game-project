3bd62b57b8b41c66137da061cd22518c9059924d
Mattias Lundkvist <mindfalls@hotmail.com>
2023-07-27 10:23:06 +0000

    define roll function

--- run.py
+++ run.py
@@ -1,3 +1,8 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+def roll():
+    min_value = 1
+    max_value = 6
+    roll = random.randint(min_value, max_value)
+
+    return roll
